6b39ea4f69ba69944d27ba5896382de1b2d0b807
as6500 <89478728+as6500@users.noreply.github.com>
2025-12-11 12:27:54 +0000

    trying something to fix the stupid error i have but ill go back if this doesnt work

--- server/main.js
+++ server/main.js
@@ -88,6 +88,7 @@ app.post("/insertGameState", (req, res) => {
 
 
 // listen for requests on port 
-app.listen(4000, () => {
-    console.log("🙌 Server is running on port 4000. Check http://localhost:4000/")
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}`)
+);
